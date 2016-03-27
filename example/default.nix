c2cdd23b0f3881adf956ca4539ab6ffc890ef484
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-03-27 11:26:59 +0100

    Make the example nix more sensible

--- example/default.nix
+++ example/default.nix
@@ -1,29 +1,24 @@
 { myWebApp ? { outPath = ./.; name = "myWebApp"; }
 , pkgs ? import <nixpkgs> {}
 }:
-let
+
+pkgs.stdenv.mkDerivation {
+  name = "my-web-app-frontend";
+  src = myWebApp;
+
+  buildInputs = [ pkgs.nodePackages.gulp ];
+
   bowerComponents = pkgs.buildBowerComponents {
     name = "my-web-app";
     generated = ./bower-packages.nix;
     src = myWebApp;
   };
 
-  frontend = pkgs.stdenv.mkDerivation {
-     name = "my-web-app-frontend";
-     src = myWebApp;
+  buildPhase = ''
+    # gulp build using copied in bower_components
+    cp  --reflink=auto --no-preserve=mode -R $bowerComponents/bower_components .
+    HOME=$PWD ${pkgs.nodePackages.gulp}/bin/gulp build
+  '';
 
-    buildInputs = [
-      pkgs.nodePackages.gulp
-      bowerComponents
-    ];
-
-    buildPhase = ''
-      # gulp build using copied in bower_components
-      cp  --reflink=auto --no-preserve=mode -R ${bowerComponents}/bower_components .
-      HOME=$PWD ${pkgs.nodePackages.gulp}/bin/gulp build
-    '';
-
-    installPhase = "mv gulpdist $out";
-  };
-in
- frontend
+  installPhase = "mv gulpdist $out";
+}
